f768515f2c558a186141fdd375beea3e78f75225
Jamie Sullivan-Phillips <jamiesullphill@gmail.com>
2025-03-29 20:45:31 -0700

    added comments and re-arranged code into relevant blocks

--- dice-block-dungeons/Scripts/block_manager.gd
+++ dice-block-dungeons/Scripts/block_manager.gd
@@ -12,6 +12,9 @@ signal new_slot_purchaed(coins : int)
 
 var selected_object = null
 
+##
+## GENERAL SECTION
+##
 # Main Entry Point
 func handle_mouse_click(mouse_pos: Vector2):
 	var topmost_object = get_topmost_object_at(mouse_pos)
@@ -28,13 +31,55 @@ func handle_mouse_click(mouse_pos: Vector2):
 	handle_backpack_tile_clicked(mouse_pos)
 
 
-func _on_spawn_die_button_pressed() -> void:
-	var die = die_scene.instantiate()
-	add_child(die)
-	die.set_random_value()
-	die.die_dropped.connect(_on_die_dropped)
+func get_topmost_object_at(mouse_pos: Vector2) -> Node2D:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = mouse_pos
+	query.collide_with_bodies = true
+	query.collide_with_areas = true
 
+	var results = space_state.intersect_point(query)
+	if results.is_empty():
+		return null
+
+	results.sort_custom(func(a, b): 
+		return (
+		(a.collider.is_in_group("Dice") and not b.collider.is_in_group("Dice")) or 
+		(a.collider.is_in_group("Dice") == b.collider.is_in_group("Dice") and a.collider.z_index > b.collider.z_index) or 
+		(a.collider.is_in_group("Dice") == b.collider.is_in_group("Dice") and a.collider.z_index == b.collider.z_index and a.collider.get_index() < b.collider.get_index())
+	))
+
+	var topmost_object = results[0].collider.get_parent()
 	
+	var i = 0
+	for object in results:
+		print("[", i, "]: ", object.collider.get_parent())
+		i += 1
+	
+	return topmost_object if topmost_object and (topmost_object.is_in_group("Blocks") or topmost_object.is_in_group("Dice")) else null
+
+
+##
+## ADD BACKPACK TILES SECTION
+##
+func _on_show_add_tiles_pressed() -> void:
+	if not backpack.are_new_tiles_shown:
+		backpack.show_add_tiles()
+	else:
+		backpack.hide_add_tiles()
+
+
+func handle_backpack_tile_clicked(mouse_pos: Vector2) -> void:
+	if backpack.is_position_add_new_block(mouse_pos):
+		if coins > 0:
+			backpack.add_new_block(mouse_pos)
+			coins -= 1
+			new_slot_purchaed.emit(coins)
+
+
+##
+## BLOCK SECTION
+##
 func _on_spawn_block_button_pressed():
 	var block = block_list.pick_random().instantiate()
 	add_child(block)
@@ -72,43 +117,6 @@ func _on_slot_overflowed(overflow_value : int, block : Block):
 		var adjacent_backpack_slots_global_positions = backpack.get_adjacent_backpack_slots_global_positions(slot_position)
 		for g_position in adjacent_backpack_slots_global_positions:
 			die_dropped_at_position(g_position, overflow_value)
-
-
-func handle_backpack_tile_clicked(mouse_pos: Vector2) -> void:
-	if backpack.is_position_add_new_block(mouse_pos):
-		if coins > 0:
-			backpack.add_new_block(mouse_pos)
-			coins -= 1
-			new_slot_purchaed.emit(coins)
-
-
-func get_topmost_object_at(mouse_pos: Vector2) -> Node2D:
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsPointQueryParameters2D.new()
-	query.position = mouse_pos
-	query.collide_with_bodies = true
-	query.collide_with_areas = true
-
-	var results = space_state.intersect_point(query)
-	if results.is_empty():
-		return null
-
-	results.sort_custom(func(a, b): 
-		return (
-		(a.collider.is_in_group("Dice") and not b.collider.is_in_group("Dice")) or 
-		(a.collider.is_in_group("Dice") == b.collider.is_in_group("Dice") and a.collider.z_index > b.collider.z_index) or 
-		(a.collider.is_in_group("Dice") == b.collider.is_in_group("Dice") and a.collider.z_index == b.collider.z_index and a.collider.get_index() < b.collider.get_index())
-	))
-
-	var topmost_object = results[0].collider.get_parent()
-	
-	var i = 0
-	for object in results:
-		print("[", i, "]: ", object.collider.get_parent())
-		i += 1
-	
-	return topmost_object if topmost_object and (topmost_object.is_in_group("Blocks") or topmost_object.is_in_group("Dice")) else null
-
 
 
 func get_topmost_block_at(mouse_pos: Vector2) -> Block:
@@ -153,6 +161,16 @@ func _on_block_dropped(block: Block):
 		print("Cannot drop block here!")
 
 
+##
+## DICE SECTION
+##
+func _on_spawn_die_button_pressed() -> void:
+	var die = die_scene.instantiate()
+	add_child(die)
+	die.set_random_value()
+	die.die_dropped.connect(_on_die_dropped)
+
+
 func _on_die_dropped(die : Die) -> void:
 	var die_position = die.global_position
 	
@@ -164,6 +182,10 @@ func _on_die_dropped(die : Die) -> void:
 		is_die_slot_at_position = block_at_position.is_position_a_die_slot(die_position)
 	
 	if is_die_slot_at_position and block_at_position.is_slotted:
+		## The resolution of die in slot placement should happen in rounds
+		## outside of the block script. The rounds should be calculated and
+		## resolved within block_manager. Block script should only be
+		## used for the visual updates.
 		block_at_position.die_placed_in_slot(die.value)
 		die.queue_free()
 
@@ -178,11 +200,3 @@ func die_dropped_at_position(die_position : Vector2, value : int) -> void:
 	
 	if is_die_slot_at_position and block_at_position.is_slotted:
 		block_at_position.die_placed_in_slot(value)
-
-
-
-func _on_show_add_tiles_pressed() -> void:
-	if not backpack.are_new_tiles_shown:
-		backpack.show_add_tiles()
-	else:
-		backpack.hide_add_tiles()
